94b5761e9a3c581e0a58039b25412d2b5fbe3aa5
Omar El Kamili <omar.elkamili@gmail.com>
2024-04-29 14:13:55 +0200

    add: comments

--- js/script.js
+++ js/script.js
@@ -11,17 +11,22 @@ createApp ({
     },
 
     created() {
+        //ciclo for che mi ripete la function generateMail
         for (let i = 0; i < 10; i++) {
             this.generateMail();
         }
     },
     methods: {
+        // function che mi pesca una mail dal server e la pusha in un arrai chiamato mailArray
         generateMail: function () {
             axios
+            // qui chiamo il server
               .get("https://flynn.boolean.careers/exercises/api/random/mail")
-              .then((resp) => {
+              .then((resp) => {                                         //qui ordino le istruzioni da seguire una volta chiamato il server
                 console.log(resp);
+                // qui assegno il valore restituito a me interessato, il quale è la mail, ad una variabile inizialmenrte vuota
                 this.mail = resp.data.response;
+                // qui pusho il valore ottenuto all'interno di un array mailArray
                 this.mailArray.push(this.mail);
               });
           },

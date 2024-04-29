51c105094cf0b2b5ebc0048b92a1c098df51f806
Omar El Kamili <omar.elkamili@gmail.com>
2024-04-29 14:09:21 +0200

    add: successfully received a response from server for one email

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-console.log("ciao");
 
-const {createAPP} = Vue;
+
+const {createApp} = Vue;
 
 createApp ({
     data() {
         return {
-            
+            mail:"",
+            mailArray:[],
         }
     },
+
+    created() {
+        this.generateMail();
+    },
     methods: {
-        
+        generateMail: function () {
+            axios
+              .get("https://flynn.boolean.careers/exercises/api/random/mail")
+              .then((resp) => {
+                console.log(resp);
+                this.mail = resp.data.response;
+                this.mailArray.push(this.mail);
+              });
+          },
     },
 
 }).mount("#app")
